3af8b1c7d63dcb42a215f14c51b1edcf66193130
fwilbor <franklin.cfg@gmail.com>
2019-08-11 00:35:25 -0400

    add Word Constructor function that will track user Guesses

--- word.js
+++ word.js
@@ -1,1 +1,26 @@
 var Letter = require("./letter");
+
+function Word(answer) {
+    this.objectArray = [];
+
+    for (var i = 0; i < answer.length; i++) {
+        var letter = new Letter(answer[i]);
+        this.objectArray.push(letter);
+    }
+
+    this.log = function () {
+        var answerLog = "";
+        for (var i = 0; i < this.objectArray.length; i++) {
+            answerLog += this.objectArray[i] + " ";
+        }
+    };
+
+    this.userGuess = function (input) {
+        for (var i = 0; i < this.objectArray.length; i++) {
+            this.objectArray[i].guess(input);
+        }
+    };
+
+}
+
+module.exports = Word;
